ae2ea418ccbb82749246684332cb9f43eb157052
justin connors <justinconnors@hotmail.ca>
2023-12-12 20:31:10 -0400

    index, and weather views added, app post and gets added

--- app.rb
+++ app.rb
@@ -6,3 +6,31 @@ require 'dotenv/load'
 API_KEY = ENV['API_KEY']
 
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+get '/' do
+    erb :index
+end
+
+post '/weather' do
+    city = params[:city]
+    redirect to("/weather/#{city}")
+end
+
+get '/weather/:city' do
+    city = params[:city]
+    @weather_data = fetch_weather(city)
+    erb :weather
+end
+
+def fetch_weather(city)
+    response = HTTParty.get("#{BASE_URL}?q=#{city}&appid=#{API_KEY}&units=metric")
+    return {} unless response.success?
+
+    weather_data = JSON.parse(response.body) 
+    {
+        name: weather_data['name'],
+        temperature: weather_data['main']['temp'],
+        description: weather_data['weather'][0]['description'],
+    }
+
+end
